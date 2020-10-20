3fb8ec1846984b340e45667a63b7dff08be971d9
Edona <dona_ez27@hotmail.com>
2020-10-21 00:49:52 +0200

    Deleted unnecessary files

--- src/script.sh
+++
@@ -1,8 +0,0 @@
-cd `pwd`
-cd src/
-hadoop com.sun.tools.javac.Main *.java
-jar cf ../exercise_1.jar *.class
-cd ..
-hadoop jar exercise_1.jar ChiSquareCalculation /user/pknees/amazon-reviews/full/reviewscombined.json /user/e11946217/Exercise1/Results -top 200 -skip /user/e11946217/Exercise1/stopwords
-hadoop fs -cat /user/e11946217/Exercise1/Results/part-r-00000 /user/e11946217/Exercise1/Results/all_terms/part-r-00000 | hadoop fs -put - /user/e11946217/Exercise1/Results
-hadoop fs -getmerge /user/e11946217/Exercise1/Results/- ~/Exercise1/output.txt
